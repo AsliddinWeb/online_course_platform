d509a464aa1c7a55f95eb7a55cebbfa3de5c1dbe
AsliddinWeb <asliddinsinger@gmail.com>
2026-01-16 01:09:19 +0500

    prod settings updated 3

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -6,16 +6,15 @@ DEBUG = False
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # CSRF trusted origins
 CSRF_TRUSTED_ORIGINS = [
-    'http://164.92.240.184:8001',
-    'http://courses.asliddin.me',
     'https://courses.asliddin.me',
+    'https://www.courses.asliddin.me',
 ]
 
 # HTTPS
-# SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
